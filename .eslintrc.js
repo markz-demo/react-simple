f5ce7c86a099cef5d57527483fd8d60b67a41204
markz-demo <markz_demo@163.com>
2023-02-15 16:42:37 +0800

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,8 +30,40 @@ module.exports = {
     __dirname: "readonly",
   },
   "rules": {
+    // off some recommendeds
     'react/react-in-jsx-scope': "off",
+    'react/prop-types': "off",
+
+    // hooks
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+
+
+    // enhances error
+    "semi": "error",
+    "eqeqeq": ["error", "always", { "null": "ignore" }],
+    "comma-dangle": ["error", "only-multiline"],
+    "eol-last": "error",
+    "radix": "error",
+    "no-const-assign": "error",
+    "no-eval": "error",
+    "no-implied-eval": "error",
+    "no-use-before-define": ["error", { "functions": false, "classes": false, "variables": false }],
+    "no-self-compare": "error",
+    "guard-for-in": "error",
+    "default-param-last": "error",
+    "array-callback-return": ["error", { "allowImplicit": false, "checkForEach": false }],
+    "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true, "allowTaggedTemplates": true }],
+    "no-shadow": "error",
+    "no-duplicate-imports": "error",
+
+    // enhances warn
+    "no-unused-vars": ["warn", {
+      "args": "none",
+      "caughtErrors": "none",
+      "ignoreRestSiblings": true,
+      "vars": "all",
+    }],
+
   }
-}
+};
